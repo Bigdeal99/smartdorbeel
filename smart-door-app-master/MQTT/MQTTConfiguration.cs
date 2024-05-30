401f276bbf8a3fd21f0979488600664efa4e5980
Marcelo Hani <90682271+Bigdeal99@users.noreply.github.com>
2024-05-30 18:22:14 +0200

    d

--- smart-door-app-master/MQTT/MQTTConfiguration.cs
+++ smart-door-app-master/MQTT/MQTTConfiguration.cs
@@ -5,7 +5,7 @@ public static class MQTTConfiguration
     public static string Server => "mqtt.flespi.io";
     public static int Port => 8883;
     public static string ClientId => "BMClient";
-    public static string Username => "9HhdWDzNukP8NL0bJIWB51TA9rsKnlwxfZLQCVQvq2bB5jvXuZr5geKscWia5ATN";
+    public static string Username => "XZdrA3Fg1uvUT0OBDRWrsJMHXGFYFp9XrRg04fl7Z1NYzj3B9joYPAdss1wbmlg3";
     public static string Password => "";
 
 }
